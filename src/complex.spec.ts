763e222a5f70b42b2fc39980d6ce3f3b14a74624
David Losert <david.losert@virtual-identity.com>
2017-10-20 08:44:39 +0200

    Implemented first simple uses cases for ramda

---
+++ src/complex.spec.ts
@@ -0,0 +1,8 @@
+import {map, reduce, forEach, pipe, filter} from 'ramda';
+import {expect} from 'chai';
+
+describe('Complex FP with Ramda', () => {
+ it('makes conditionals super easy to read', () => {
+
+ });
+});
---
+++ src/simple.spec.ts
@@ -0,0 +1,36 @@
+import {map, reduce, forEach, pipe, filter} from 'ramda';
+import {expect} from 'chai';
+
+describe('Simple FP', () => {
+  it('uses all common iterator functions equally', () => {
+	const data = [1, 2, 3];
+
+	const resultOld = data.map((num: number) => `Test${num}`);
+	const resultNew = map((num: number) => `Test${num}`, data);
+
+	expect(resultOld).to.eql(resultNew);
+  });
+
+
+  const prependTestForAll = map((num: number) => `Test${num}`);
+  it('functions can be prepared before using with ramda', () => {
+	const result = prependTestForAll([1, 2, 3]);
+	expect(result).to.eql(['Test1', 'Test2', 'Test3']);
+  });
+
+
+  const isEven = num => num % 2 === 0;
+  const filterEven = filter(isEven);
+  const prependTestForEven = pipe(filterEven, prependTestForAll);
+
+  it('function can be composed', () => {
+	const result = prependTestForEven([1, 2, 4]);
+
+	expect(result).to.eql(['Test2', 'Test4']);
+  });
+
+
+  it('works as well for non-array functions', () => {
+
+  });
+});
